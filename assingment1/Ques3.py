0ce8d40e6cdd1afc3a10691fe8c2df4d226b9ff9
Anil maharjan <anilmaharjan995@gmail.com>
2021-07-06 23:20:44 +0545

    question no 3 added in assingment 1

---
+++ assingment1/Ques3.py
@@ -0,0 +1,4 @@
+# Write a Python code that takes the degree as input from the user and convert it into radian.
+degree=int(input("Enter degree to convert it to radian:"))
+radian=degree*(22/7/180)
+print("Radian of given degree is:",format(radian,".4f"))
